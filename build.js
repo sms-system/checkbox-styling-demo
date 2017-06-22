c0daba432a14d09716db8fd0252883b289d8275d
Serge Smirnov <sms-system@mail.ru>
2017-06-22 21:09:46 +0600

    Initial commit

---
+++ build.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack')
+const webpackConfig = require('./webpack.config')
+
+webpack(webpackConfig, function (err, stats) {
+  if (err) throw err
+  process.stdout.write(stats.toString({
+    colors: true,
+    modules: false,
+    children: false,
+    chunks: false,
+    chunkModules: false
+  }) + '\n\n')
+
+  console.log('Build complete.')
+})
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const fs = require("fs")
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const WebpackHtmlGeneratorPlugin = require('webpack-html-generator-plugin')
+
+module.exports = {
+  entry: './src/style.styl',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: ''
+  },
+  module: {
+    rules: [
+      {
+        test: /\.styl$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'postcss-loader'
+            },
+            {
+              loader: 'stylus-loader'
+            },
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("style.css"),
+    new WebpackHtmlGeneratorPlugin.WebpackHtmlGeneratorPlugin({
+      template: fs.readFileSync('./src/index.html', 'utf8'),
+      compress: false
+    }),
+  ]
+}
